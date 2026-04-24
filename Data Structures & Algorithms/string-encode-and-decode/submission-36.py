349cb058562079638565387ae5137e0a8d22745f
Tim <74032068+TimWJT@users.noreply.github.com>
2026-04-24 13:06:42 +1000

    Add: string-encode-and-decode - submission-36

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-36.py
@@ -0,0 +1,34 @@
+class Solution:
+
+    def encode(self, strs: List[str]) -> str:
+        encoded_arr = []
+        for s in strs:
+            # Add the length, a '#' delimiter, and the word
+            encoded_arr.append(str(len(s)) + "#" + s)
+            
+        return "".join(encoded_arr)
+
+    def decode(self, s: str) -> List[str]:
+        output = []
+        i = 0
+        
+        while i < len(s):
+            # 1. Find where the '#' is
+            j = i
+            while s[j] != "#":
+                j += 1
+                
+            # 2. The number before the '#' is our length
+            length = int(s[i:j])
+            
+            # 3. The word starts right after the '#'
+            word_start = j + 1
+            word_end = word_start + length
+            
+            # 4. Slice the word out and add it to our output
+            output.append(s[word_start : word_end])
+            
+            # 5. Jump 'i' forward to the start of the next number
+            i = word_end
+            
+        return output
